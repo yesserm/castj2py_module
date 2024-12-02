806e7e9759bd25b4674f12df83b245a016356fc9
Yesser Miranda <yesser.miranda0106@gmail.com>
2024-12-02 12:59:11 -0600

    feat(package|init) - add initial structure to bot cast json bot 2 python

---
+++ __init__.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+    
+    pip install <package> -t .
+
+"""
+
+"""
+    Obtengo el modulo que fue invocado
+"""
+module = GetParams("module")
+
+
+if module == "convertJson2Py":
+
+    print('Bot creado')
+
+
+
